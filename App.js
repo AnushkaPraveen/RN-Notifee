de0576feee5127519ad2b2c74e7b754e264f5a71
AnushkaPraveen <anushkapraveen98@gmail.com>
2022-03-25 09:36:00 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,94 @@
+
+
+import React,{useEffect} from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button
+} from 'react-native';
+import notifee from '@notifee/react-native';
+import {display,getNotification} from './app/notification';
+
+
+
+
+const App = () => {
+  
+useEffect(()=>{
+ display()
+ 
+},[])
+
+const testNotification=()=>{
+  const data={
+    channelId:'test',
+    name:'default',
+    title:'App Notification',
+    body:'This is notifciation pass by function '
+  }
+  getNotification(data);
+}
+
+
+  const onDisplayNotification=async()=>{
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    await notifee.requestPermission();
+   await notifee.displayNotification({
+     
+      title: 'Notification Title',
+      body: 'Main body content of the notification',
+      android: {
+        channelId,
+        smallIcon: 'ic_launcher', // optional, defaults to 'ic_launcher'.
+        
+        
+      },
+    });
+
+ 
+
+
+  }
+
+  return (
+    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+    
+    <Text>Hello</Text>
+    <Button title="get notification" onPress={onDisplayNotification}/>
+    <View style={{marginTop:10}}>
+    <Button title="Notification Function" onPress={testNotification}/>
+    </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ app/notification.js
@@ -0,0 +1,54 @@
+
+import notifee,{AndroidStyle,AndroidColor,EventType} from '@notifee/react-native';
+
+export const display=()=>{
+    console.log('====================================');
+   
+    console.log('Notification Functions');
+    console.log('====================================');
+}
+
+
+export const getNotification=async(data)=>{
+
+notifee.onForegroundEvent(({type,detail})=>{
+    console.log(type);
+    if(type==EventType.ACTION_PRESS && detail.pressAction.id){
+        console.log('user pressed aan action with id:',detail.pressAction.id);
+    }
+})
+
+
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+  
+   await notifee.displayNotification({
+     
+      title: data.title,
+      body: data.body,
+      android: {
+        channelId,
+        smallIcon: 'ic_launcher', // optional, defaults to 'ic_launcher'.
+        style: {type: AndroidStyle.BIGTEXT, text: 'This is notifciation pass by function bncgt dytyff vdyy dyyydud ddty dydd dyddttfu fytut '  },
+        showTimestamp: true,
+        color:'#E8210C',
+        actions:[
+            {
+                title:'View',
+                pressAction:{
+                    id:'open'
+                }
+            },
+            {
+                title:'Read',
+                pressAction:{
+                    id:'read'
+                }
+            }
+        ]
+        
+      },
+    })}
