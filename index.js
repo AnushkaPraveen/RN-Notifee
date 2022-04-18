85ff03774745067205077a552fff4ecc108786e0
AnushkaPraveen <anushkapraveen98@gmail.com>
2022-04-18 17:05:52 +0530

    add background run on index.js

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import notifee, { EventType } from '@notifee/react-native';
+import messaging from '@react-native-firebase/messaging';
 
-
+// Register background handler
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
 
 notifee.onBackgroundEvent(({ type, detail }) => {
     if (type === EventType.ACTION_PRESS && detail.pressAction.id) {
