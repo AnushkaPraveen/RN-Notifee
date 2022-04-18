8fd2d5459a9e276cd5efadbb8832f5dd939086bf
AnushkaPraveen <anushkapraveen98@gmail.com>
2022-04-18 15:16:59 +0530

    add switch case to foreground and background events

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import notifee, { EventType } from '@notifee/react-native';
+import {notificationPress,actionPress} from './app/function';
 
 
 
-notifee.onBackgroundEvent(({ type, detail }) => {
+notifee.onBackgroundEvent(async({ type, detail }) => {
+  switch(type){
+    case EventType.ACTION_PRESS:
+      actionPress(detail.pressAction.id);
+      break;
+    case EventType.PRESS:
+      notificationPress()
+      break;
+    case EventType.DISMISSED:
+      console.log('User dismissed notification',detail.notification);
+      alert(detail.notification.title)
+      break;
+  }
+})
+
+/* notifee.onBackgroundEvent(({ type, detail }) => {
     if (type === EventType.ACTION_PRESS && detail.pressAction.id) {
       console.log('User pressed an action with the id: ', detail.pressAction.id);
     }
@@ -22,6 +38,6 @@ notifee.onBackgroundEvent(({ type, detail }) => {
       console.log('User dismissed notification',detail.notification);
       alert(detail.notification.title)
     }
-  });
+  }); */
 
 AppRegistry.registerComponent(appName, () => App);
